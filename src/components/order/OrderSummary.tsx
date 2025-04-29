543eb789e8825a9c32d5042463e2963a03d477a4
EmmVega <emmanuelvegamecinas@gmail.com>
2025-04-29 14:21:56 -0600

    feat: implement Zustand store for order management and update OrderSummary component

--- src/components/order/OrderSummary.tsx
+++ src/components/order/OrderSummary.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+'use client'
+import { useStore } from '../../../lib/store'
 
 const OrderSummary = () => {
+  const order = useStore((state) => state.order);
+
   return (
     <aside className='lg:h-screen lg:overflow-y-scroll md:w-64 lg:w-96 p-5'>
       <h1 className='text-4xl text-center font-black'>Mi pedido</h1>
+
+      {order.length === 0 ? <p className='text-center my-10'>Carro vacío</p> : (
+        <div className='mt-5'>
+          <p>Si hay algo</p>
+        </div>
+      )}
     </aside>
   )
 }
