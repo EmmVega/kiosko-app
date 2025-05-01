e28a479113b1b9b2d30913eccb2b2f052e40847b
EmmVega <emmanuelvegamecinas@gmail.com>
2025-04-30 22:51:57 -0600

    admin/orders + logo component

--- src/components/order/OrderSidebar.tsx
+++ src/components/order/OrderSidebar.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { prisma } from '../../../prisma/lib/prisma';
 import CategoryIcon from '../ui/CategoryIcon';
+import Logo from '../ui/Logo';
 
 
 async function getCategories() {
@@ -13,6 +14,7 @@ const OrderSidebar = async () => {
 
   return (
     <aside className='md:w-72 md:h-screen bg-white'>
+        <Logo/>
         <nav className='mt-10'>
          {categories.map(category => (
           <CategoryIcon 
