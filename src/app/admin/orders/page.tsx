232e76f7983fb88e0ed559d01b643852c6ffd674
EmmVega <emmanuelvegamecinas@gmail.com>
2025-05-01 16:19:31 -0600

    orderCard details

--- src/app/admin/orders/page.tsx
+++ src/app/admin/orders/page.tsx
@@ -1,12 +1,50 @@
 import Heading from '@/components/ui/Heading'
 import React from 'react'
+import { prisma } from '../../../../prisma/lib/prisma'
+import OrderCard from '@/components/order/OrderCard';
 
-const page = () => {
+async function getPendingOrders () {
+  const orders = await prisma.order.findMany({
+    where: {
+      status: false 
+    },
+    include: {
+      orderProducts: {
+        include: {
+          product: true
+        }
+      }
+    }
+  })
+
+  return orders;
+}
+
+const OrdersPage = async () => {
+  const orders = await getPendingOrders();
+  console.log(orders);
   return (
+    <>
     <Heading>
       Administrar Ordenes
     </Heading>
+
+    {orders.length ? (
+      <div
+        className='grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-5 mt-5'
+      >
+        {orders.map(order => (
+          <OrderCard 
+            key={order.id}
+            order={order}
+          />
+        ))}
+      </div>
+    ) : <p className='text-center'>No hay Ordenes Pendientes</p>
+
+    }
+    </>
   )
 }
 
-export default page
+export default OrdersPage
